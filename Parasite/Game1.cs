0de362e2a0b6819ae7991d003a6f3f3874ca9295
Darrel-Day Guerrero <ddayy.guerrero@hotmail.com>
2015-01-25 22:44:50 -0500

    Import via content pipeline and load textures

--- Parasite/Game1.cs
+++ Parasite/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using System;
 namespace Parasite
 {
     /// <summary>
@@ -8,42 +9,24 @@ namespace Parasite
     /// </summary>
     public class Game1 : Game
     {
+        private Map _map;
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        Vector3 eye;
-        Vector3 up;
-        Vector3 at;
+        SpriteBatch _spriteBatch;
+    
+        // Textures
+        private Texture2D _northOpenWall;
+        private Texture2D _northClosedWall;
+        private Texture2D _eastOpenWall;
+        private Texture2D _eastClosedWall;
+        private Texture2D _southOpenWall;
+        private Texture2D _southClosedWall;
+        private Texture2D _westOpenWall;
+        private Texture2D _westClosedWall;
 
-        float fov;
-        //camera properties
-        float zNear;
-        float zFar;
+        private Texture2D _floorTexture;
 
-        BasicEffect basicEffect;
-        DynamicVertexBuffer vertexBuffer;
-        DynamicIndexBuffer indexBuffer;
-        VertexPositionColor[] cube;
-        float cubeRotation = 0.0f;
-        static ushort[] cubeIndices =
-    		{
-    			0,2,1, // -x
-    			1,2,3,
+        private readonly Rectangle _screenBounds;
 
-    			4,5,6, // +x
-    			5,7,6,
-
-    			0,1,5, // -y
-    			0,5,4,
-
-    			2,6,7, // +y
-    			2,7,3,
-
-    			0,4,6, // -z
-    			0,6,2,
-
-    			1,3,7, // +z
-    			1,7,5,
-    		};
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -58,33 +41,7 @@ namespace Parasite
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            zNear = 0.001f;
-            zFar = 1000.0f;
-            fov = MathHelper.Pi * 70.0f / 180.0f;
-            eye = new Vector3(0.0f, 0.7f, 1.5f);
-            at = new Vector3(0.0f, 0.0f, 0.0f);
-            up = new Vector3(0.0f, 1.0f, 0.0f);
-
-            cube = new VertexPositionColor[8];
-            cube[0] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, -0.5f), new Color(0.0f, 0.0f, 0.0f));
-            cube[1] = new VertexPositionColor(new Vector3(-0.5f, -0.5f, 0.5f), new Color(0.0f, 0.0f, 1.0f));
-            cube[2] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, -0.5f), new Color(0.0f, 1.0f, 0.0f));
-            cube[3] = new VertexPositionColor(new Vector3(-0.5f, 0.5f, 0.5f), new Color(0.0f, 1.0f, 1.0f));
-            cube[4] = new VertexPositionColor(new Vector3(0.5f, -0.5f, -0.5f), new Color(1.0f, 0.0f, 0.0f));
-            cube[5] = new VertexPositionColor(new Vector3(0.5f, -0.5f, 0.5f), new Color(1.0f, 0.0f, 1.0f));
-            cube[6] = new VertexPositionColor(new Vector3(0.5f, 0.5f, -0.5f), new Color(1.0f, 1.0f, 0.0f));
-            cube[7] = new VertexPositionColor(new Vector3(0.5f, 0.5f, 0.5f), new Color(1.0f, 1.0f, 1.0f));
-
-            vertexBuffer = new DynamicVertexBuffer(graphics.GraphicsDevice, typeof(VertexPositionColor), 8, BufferUsage.WriteOnly);
-            indexBuffer = new DynamicIndexBuffer(graphics.GraphicsDevice, typeof(ushort), 36, BufferUsage.WriteOnly);
-
-            basicEffect = new BasicEffect(graphics.GraphicsDevice); //(device, null);
-            basicEffect.LightingEnabled = false;
-            basicEffect.VertexColorEnabled = true;
-            basicEffect.TextureEnabled = false;
-
-            graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+           
             base.Initialize();
         }
 
@@ -95,8 +52,23 @@ namespace Parasite
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            // TODO: use this.Content to load your game content here
+            var spriteBatch = new SpriteBatch(GraphicsDevice);
+           
+            // Load Textures
+            _northOpenWall = Content.Load<Texture2D>("north-open");
+            _northClosedWall = Content.Load<Texture2D>("north-closed");
+            _eastOpenWall = Content.Load<Texture2D>("east-open");
+            _eastClosedWall = Content.Load<Texture2D>("east-closed");
+            _southOpenWall = Content.Load<Texture2D>("south-open");
+            _southClosedWall = Content.Load<Texture2D>("south-closed");
+            _westOpenWall = Content.Load<Texture2D>("west-open");
+            _westClosedWall = Content.Load<Texture2D>("west-closed");
+
+            _floorTexture = Content.Load<Texture2D>("floor");
+
+            // Setup the map. 
+            _map = new Map(Environment.TickCount);  
+
         }
 
         /// <summary>
@@ -116,7 +88,6 @@ namespace Parasite
         protected override void Update(GameTime gameTime)
         {
             // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
@@ -126,32 +97,28 @@ namespace Parasite
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // Compute camera matrices.
-            Matrix View = Matrix.CreateLookAt(eye, at, up);
-
-            Matrix Projection = Matrix.CreatePerspectiveFieldOfView(fov, GraphicsDevice.Viewport.AspectRatio, zNear, zFar);
-            cubeRotation += (0.001f) * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            Matrix World = Matrix.CreateRotationY(cubeRotation);
-            // TODO: Add your drawing code here
-
-            vertexBuffer.SetData(cube, 0, 8, SetDataOptions.Discard);
-            indexBuffer.SetData(cubeIndices, 0, 36, SetDataOptions.Discard);
-
-            GraphicsDevice device = basicEffect.GraphicsDevice;
-            device.SetVertexBuffer(vertexBuffer);
-            device.Indices = indexBuffer;
-
-            basicEffect.View = View;
-            basicEffect.Projection = Projection;
-            basicEffect.World = World;
-            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 8, 0, 36);
-            }
             base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void DrawRoom()
+        {
+            var screen = _screenBounds;
+            var center = screen.Center.ToVector2();
+            var room = _map.PlayerRoom;
+
+            _spriteBatch.Begin();
+
+            // Draw the floor
+            _spriteBatch.Draw(_floorTexture, center - new Vector2(_floorTexture.Width / 2, _floorTexture.Height / 2), Color.White);
+
+            var roomHalfWidth = _northClosedWall.Width / 2.0f;
+            var wallDepth = _northClosedWall.Height;
+            var roomHalfHeight = _eastClosedWall.Height / 2.0f; 
+
         }
     }
 }
